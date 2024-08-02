30430a8dde5089ce4200936d3bfbfe34922cb717
Martin Gofman <martin2500@yandex.ru>
2024-08-01 22:33:00 -0400

    RestTemplate learning

--- src/main/java/com/storage/mystorage/myControllers/CreationController.java
+++ src/main/java/com/storage/mystorage/myControllers/CreationController.java
@@ -3,10 +3,13 @@ package com.storage.mystorage.myControllers;
 import com.storage.mystorage.myEntitys.Storage;
 import com.storage.mystorage.services.EntityRepos.StorageService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @AllArgsConstructor
@@ -15,11 +18,22 @@ public class CreationController {
 
     final StorageService storageService;
 
+    final RestTemplate restTemplate;
+
     @PostMapping("/storage")
-    public Storage newStorage(@RequestBody Storage request) {
+    public ResponseEntity<?> newStorage(@RequestBody Storage request) {
         Storage storage = new Storage();
         storage.setName(request.getName());
         storageService.saveStorage(storage);
-        return storage;
+
+        String url = "http://localhost:8080/new/respond";
+
+        Storage response = restTemplate.postForObject(url, storage, Storage.class);
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/respond")
+    public ResponseEntity<?> respond(){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/storage/mystorage/myControllers/GlobalExceptionHandler.java
+++ src/main/java/com/storage/mystorage/myControllers/GlobalExceptionHandler.java
@@ -1,27 +1,27 @@
-package com.storage.mystorage.myControllers;
-
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-
-//Класс, который перехватывает исключения и позволяет обрабатывать их
-@ControllerAdvice
-public class GlobalExceptionHandler {
-
-
-    //Метод в котором мы описываем что перехватывать и что делать
-    @ExceptionHandler(Exception.class)
-    //Перехватывает запрос, который пришел к нам и вызвал ошибку
-    public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
-
-        // false - не будет включать информацию о клиенте
-        String requestDetails = request.getDescription(false);
-        String errorDetails = "Internal server error: " + ex.getMessage() + " at " + requestDetails;
-
-        //оформление ответа
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-}
+//package com.storage.mystorage.myControllers;
+//
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.context.request.WebRequest;
+//
+////Класс, который перехватывает исключения и позволяет обрабатывать их
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//
+//
+//    //Метод в котором мы описываем что перехватывать и что делать
+//    @ExceptionHandler(Exception.class)
+//    //Перехватывает запрос, который пришел к нам и вызвал ошибку
+//    public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
+//
+//        // false - не будет включать информацию о клиенте
+//        String requestDetails = request.getDescription(false);
+//        String errorDetails = "Internal server error: " + ex.getMessage() + " at " + requestDetails;
+//
+//        //оформление ответа
+//        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+//}
